2d7b38844b276d9626153cfb086c6329962efd06
iCanhua <525921307@qq.com>
2019-06-24 01:19:12 +0800

    fix get task

--- schedule-sdk/src/main/java/com/evolve/schedule/sdk/session/ScheduleJob.java
+++ schedule-sdk/src/main/java/com/evolve/schedule/sdk/session/ScheduleJob.java
@@ -16,7 +16,7 @@ public class ScheduleJob {
 
   private static final String SCHEDULE_API = "/v2/engine/schedule";
 
-  private static final String READY_UNIT_API = "/v2/engine/amazon/asinStock/ready_unit";
+  private static final String READY_UNIT_API = SCHEDULE_API + "/v2/engine/{group}/{job}/ready_unit";
 
   private String SCHEDULE_URL;
 
@@ -26,63 +26,68 @@ public class ScheduleJob {
 
   private String job;
 
-  public ScheduleJob(ScheduleClient client,String group,String job){
+  public ScheduleJob(ScheduleClient client, String group, String job) {
     scheduleClient = client;
     SCHEDULE_URL = client.getServerUrl() + SCHEDULE_API;
-    GET_TASK_URL = client.getServerUrl() + READY_UNIT_API;
+    GET_TASK_URL = READY_UNIT_API.replace("{group}",group);
+    GET_TASK_URL = READY_UNIT_API.replace("{job}",job);
     this.group = group;
     this.job = job;
   }
 
-  public void start(Map<String,Object> jobDataMap) throws Exception {
+  public void start(Map<String, Object> jobDataMap) throws Exception {
     JSONArray start = new JSONArray();
-    start.add(newStart(jobDataMap,null));
+    start.add(newStart(jobDataMap, null));
     JSONObject request = new JSONObject();
-    request.put("start",start);
-    postJson(SCHEDULE_URL,request);
+    request.put("start", start);
+    postJson(SCHEDULE_URL, request);
   }
 
-  public void starts(List<Map<String,Object>> jobDataMaps) throws Exception {
+  public void starts(List<Map<String, Object>> jobDataMaps) throws Exception {
     JSONArray start = new JSONArray();
-    for(Map<String,Object> dataMap:jobDataMaps){
-      start.add(newStart(dataMap,null));
+    for (Map<String, Object> dataMap : jobDataMaps) {
+      start.add(newStart(dataMap, null));
     }
     JSONObject request = new JSONObject();
-    request.put("start",start);
-    postJson(SCHEDULE_URL,request);
+    request.put("start", start);
+    postJson(SCHEDULE_URL, request);
   }
 
-  public void schedule(Map<String,Object> jobDataMap , String cron){
+  public void schedule(Map<String, Object> jobDataMap, String cron) {
 
   }
 
   public JSONArray getTask(int size) throws Exception {
     Map params = new HashMap();
-    params.put("size",String.valueOf(size));
-    String response = HttpUtils.get(GET_TASK_URL,params,null);
-    return (JSONArray)JSONArray.parse(response);
+    params.put("size", String.valueOf(size));
+    String response = HttpUtils.get(GET_TASK_URL, params, null);
+    return (JSONArray) JSONArray.parse(response);
   }
 
-  private JSONObject newStart(Map<String,Object> jobDataMap,String trigger){
+  private JSONObject newStart(Map<String, Object> jobDataMap, String trigger) {
     JSONObject start = new JSONObject();
-    start.put("jobDetail",newJobDetail(jobDataMap));
-//    start.put("trigger","");
+    start.put("jobDetail", newJobDetail(jobDataMap));
+    //    start.put("trigger","");
     return start;
   }
 
-  private JSONObject newJobDetail(Map<String,Object> jobDataMap){
+  private JSONObject newJobDetail(Map<String, Object> jobDataMap) {
     JSONObject jobDetail = new JSONObject();
-    jobDetail.put("group",group);
-    jobDetail.put("job",job);
-    jobDetail.put("jobDataMap",jobDataMap);
+    jobDetail.put("group", group);
+    jobDetail.put("job", job);
+    jobDetail.put("jobDataMap", jobDataMap);
     return jobDetail;
   }
 
-  private String postJson(String url,JSONObject body) throws Exception {
-    Map<String,String> headers = new HashMap<>();
-    headers.put("Content-Type","application/json");
-    String response = HttpUtils.post(url, JSON.toJSONString(body, SerializerFeature.DisableCircularReferenceDetect), headers);
+  private String postJson(String url, JSONObject body) throws Exception {
+    Map<String, String> headers = new HashMap<>();
+    headers.put("Content-Type", "application/json");
+    String response = HttpUtils.post(url,
+                                     JSON.toJSONString(body,
+                                                       SerializerFeature.DisableCircularReferenceDetect),
+                                     headers);
     return response;
   }
+
 
 }
