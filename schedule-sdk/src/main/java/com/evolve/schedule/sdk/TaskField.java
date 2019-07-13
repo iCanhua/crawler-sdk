51006f9598cf52ddff6c6eb4a21ddd39f320a5d5
iCanhua <525921307@qq.com>
2019-07-13 18:44:48 +0800

    统一变量

--- schedule-sdk/src/main/java/com/evolve/schedule/sdk/TaskField.java
+++ schedule-sdk/src/main/java/com/evolve/schedule/sdk/TaskField.java
@@ -1,6 +1,6 @@
 package com.evolve.schedule.sdk;
 
-public class Constant {
+public class TaskField {
   public static final String TASK_FEATURE = "taskFeature";
   public static final String NAME_SPACE = "nameSpace";
 }
